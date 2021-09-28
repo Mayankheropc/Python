55aa19a851ee69ae7b43cca272a3813dc990cc74
Mayank Singh <67374046+Mayankheropc@users.noreply.github.com>
2021-09-28 16:35:48 +0530

    Add files via upload

---
+++ count no. of zeroes.py
@@ -0,0 +1,8 @@
+# Program to count the number of zeroes in following tuple ->  a = (7,0,8,0,0,9)
+# By - Mayank Singh
+# Date of Creation - 28-09-2021
+# Last Modified - 28-09-2021
+
+tuple_numbers = (7,0,8,0,0,9) 
+count = tuple_numbers.count(0) 
+print("The number of zeroes are ",count) 
