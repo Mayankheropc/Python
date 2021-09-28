5d899f8628df300816c7d3c145ae6f1a35491caf
Mayank Singh <67374046+Mayankheropc@users.noreply.github.com>
2021-09-28 15:20:16 +0530

    Add files via upload

---
+++ print good afternoon befor your name.py
@@ -0,0 +1,8 @@
+# python program to display a user entered name followed by 
+# Good Afternoon using input() function
+# By - Mayank Singh
+# Date of Creation - 28-09-2021
+# Last Modified - 28-09-2021
+
+name = input("enter the name : ")
+print("Good Afternoon " + name)
