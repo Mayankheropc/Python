32e9cec53e3ffae418b9bc8fc4ac01027d3fc7ec
Mayank Singh <67374046+Mayankheropc@users.noreply.github.com>
2021-09-28 16:51:46 +0530

    Add files via upload

---
+++ letter template.py
@@ -0,0 +1,9 @@
+#Write a program to fill in a letter template given below with name and date:
+# By - Mayank Singh
+# Date of Creation - 28-09-2021
+# Last Modified - 28-09-2021
+
+x = input("Enter name: ")
+y = input("Enter date: ")
+print("Dear " +  x , end="\n" + "You have selected ")
+print(end="\n" + "Date: " + y)
