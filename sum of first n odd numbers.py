d7255d20912207fb869c9a695429c1800fa39da7
Mayank Singh <67374046+Mayankheropc@users.noreply.github.com>
2021-10-10 13:17:59 +0530

    Add files via upload

---
+++ sum of first n odd numbers.py
@@ -0,0 +1,7 @@
+# python program to find sum of first n odd numbers
+# By - Mayank Singh
+# Date of Creation - 28-09-2021
+# Last Modified - 28-09-2021
+
+n = int(input("enter value of n: "))
+print("sum of first n even number = ", n*n)
