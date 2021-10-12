0b9b0e0d70c08d6c7a83906162f2df37c670a8b5
Mayank Singh <67374046+Mayankheropc@users.noreply.github.com>
2021-10-12 14:17:09 +0530

    Add files via upload

---
+++ sum of first n even numbers.py
@@ -0,0 +1,16 @@
+# python program to find sum of first n even numbers
+# By - Mayank Singh
+# Date of Creation - 28-09-2021
+# Last Modified - 28-09-2021
+
+num = int(input("Print sum of even numbers till: "))
+
+total = 0
+
+for i in range(1, num + 1):
+
+    # Check for even or not.
+    if((i % 2) == 0):
+        total = total + i
+
+print("\nSum of even numbers from 1 to", num, "is :", total)
