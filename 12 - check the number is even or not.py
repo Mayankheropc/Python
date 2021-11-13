6f2867a337ca294ce6e1252d6c36826bdd444e9b
Mayank Singh <67374046+Mayankheropc@users.noreply.github.com>
2021-11-13 20:57:50 +0530

    Add files via upload

---
+++ 12 - check the number is even or not.py
@@ -0,0 +1,11 @@
+# Python program to accept the sting of even length
+# By - Mayank Singh
+# Date of Creation - 08-10-2021
+# Last Modified - 08-10-2021
+
+num = int(input("enter a number: "))
+
+if (num%2)==0:   #checking if the number is completely divisible by 2?
+    print("The number is even")
+else:
+    print("The number is odd")
