47b09a53024f379eac1b02bac8313dc97855064a
Mayank Singh <67374046+Mayankheropc@users.noreply.github.com>
2021-11-18 00:51:07 +0530

    Add files via upload

---
+++ 40 - Program that takes input from the user and displays that input back in upper and lower cases.py
@@ -0,0 +1,20 @@
+# Write a Python program that takes input from the user and displays that input back in upper and lower cases
+
+# By - Mayank Singh
+# Date of Creation - 02-11-2021
+# Last Modified - 05-11-2021
+
+mystring = input("Enter a string: ")
+
+print("String in UPPER case is: ", mystring.upper())
+print("String in lower case is: ", mystring.lower())
+
+# Output
+
+# Enter a string: Mayank Singh
+# String in UPPER case is:  MAYANK SINGH
+# String in lower case is:  mayank singh
+
+# Enter a string: this is a DEMO sTrInG
+# String in UPPER case is:  THIS IS A DEMO STRING
+# String in lower case is:  this is a demo string
